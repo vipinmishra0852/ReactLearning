0d4b02883dc3d07b6c1d8ddf8a953c23af1b7d5b
vipinmishra0852 <vipinmishra0852@gmail.com>
2024-09-18 20:42:16 +0530

    Committed

---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hello = () => {
+  return <p>Hello Sir !!!</p>;
+};
+
+const Greet = () => {
+  return <p>Hello Greetings !!!</p>;
+};
+
+const Namaste = () => {
+  return <p>Namaste Sahab !!!</p>;
+};
+
+export default Hello;
+export { Greet, Namaste };
